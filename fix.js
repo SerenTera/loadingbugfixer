bf3401b4bf13b3ae3e93121d2cf4c63554c2d818
Seren <cmingxuan94@gmail.com>
2018-01-06 13:21:44 +0800

    Update fix.js

--- fix.js
+++ fix.js
@@ -1,11 +1,9 @@
-const eventzonebugged=7003,
-	eventzonecorrect=7031
+const eventzonecorrect=7031
+
 
 module.exports = function fixloadbug(dispatch){
 	dispatch.hook('S_LOAD_TOPO',1, event => {
-		if(event.zone==eventzonebugged) {
-			event.zone=eventzonecorrect
-			return true
-		}
+		event.zone=eventzonecorrect
+		return true
 	})
 }
