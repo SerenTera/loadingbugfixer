52d7d1b88f3a4443046509de3cf8f7f06f9514a3
Seren <cmingxuan94@gmail.com>
2018-01-06 13:24:42 +0800

    Update fix.js

--- fix.js
+++ fix.js
@@ -2,7 +2,7 @@ const eventzonecorrect=7031
 
 
 module.exports = function fixloadbug(dispatch){
-	dispatch.hook('S_LOAD_TOPO',1, event => {
+	dispatch.hookOnce('S_LOAD_TOPO',1, event => {
 		event.zone=eventzonecorrect
 		return true
 	})
